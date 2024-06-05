53134febbd9e7ba84aa2e51832cdef3531dafe87
2290140253 <2290140253@qq.com>
2024-06-05 10:49:28 +0800

    Update data.py

--- tic-tac-toe/frontends/data.py
+++ tic-tac-toe/frontends/data.py
@@ -53,3 +53,196 @@ def get_or_create_db(db_filename):
         con.execute(sql_create_GameDetail)       
         con.commit()
     return con
+
+############管理员操作#########################################################################
+def check_player_id(player_id):
+    """检查Player表中是否存在player_id"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql_pattern = '''SELECT PLAYERNAME FROM Player WHERE PLAYERID="{0}"'''
+        sql = sql_pattern.format(player_id)
+        cur = con.execute(sql)
+        row = cur.fetchone()
+        if row:
+            return row[0]
+        else:
+            return False
+    finally:
+        con.close()
+
+def get_player_list():
+    """查找数据库Player表，获取玩家信息列表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''SELECT * FROM Player'''
+        results = con.execute(sql)
+        players = results.fetchall()
+        player_list = []
+        for player in players:
+            player_list.append(player)
+        return player_list
+    finally:
+        con.close()
+        
+def get_admin_list():
+    """查找数据库Admin表，获取管理员信息列表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''SELECT * FROM Admin'''
+        results = con.execute(sql)
+        admins = results.fetchall()
+        admin_list = []
+        for admin in admins:
+            admin_list.append(admin)
+        return admin_list
+    finally:
+        con.close()
+        
+def get_game_list():
+    """查找数据库GameDetail表，获取游戏细节列表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''SELECT * FROM GameDetail'''
+        results = con.execute(sql)
+        games = results.fetchall()
+        game_list = []
+        for game in games:
+            game_list.append(game)
+        return game_list
+    finally:
+        con.close()
+
+def insert_player(player_name, password, win, tie, lose, point):
+    """插入一条记录到Player表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''INSERT INTO Player(PLAYERNAME, PASSWORD, WIN, TIE, LOSE, POINT)
+                                      VALUES (?,?,?,?,?,?)'''
+        con.execute(sql, (player_name, password, win, tie, lose, point))
+        con.commit()
+    finally:
+        con.close()
+        
+def insert_admin(administrator_name, administrator_pw):
+    """插入一条记录到Admin表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''INSERT INTO Admin(ADMINNAME, PASSWORD)
+                                      VALUES (?,?)'''
+        con.execute(sql, (administrator_name, administrator_pw))
+        con.commit()
+    finally:
+        con.close()
+        
+def insert_game(player_id1, player_name1, player_id2, player_name2, location, duration, winner, loser):
+    """插入一条记录到GameDetail表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''INSERT INTO GameDetail(PLAYERID1, PLAYERNAME1, PLAYERID2, PLAYERNAME2, DETAILTEXT, TIME, WINNER, LOSER)
+                                      VALUES (?,?,?,?,?,?,?,?)'''
+        con.execute(sql, (player_id1, player_name1, player_id2, player_name2, location, duration, winner, loser))
+        con.commit()
+    finally:
+        con.close()
+
+def update_player(player_id, player_name, password, win, tie, lose, point):
+    """更新一条记录到Player表，同时更新GameDetail的相关记录"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql_update_player = '''UPDATE Player
+                                  SET PLAYERNAME = ?
+                                      ,PASSWORD = ?
+                                      ,WIN = ?
+                                      ,TIE = ?
+                                      ,LOSE = ?
+                                      ,POINT = ?
+                                  WHERE PLAYERID = ?'''
+        con.execute(sql_update_player, (player_name, password, win, tie, lose, point, player_id))
+        sql_update_game_detail = '''UPDATE GameDetail
+                                       SET PLAYERNAME1 = ?
+                                       WHERE PLAYERID1 = ?'''
+        con.execute(sql_update_game_detail, (player_name, player_id))
+        sql_update_game_detail2 = '''UPDATE GameDetail
+                                       SET PLAYERNAME2 = ?
+                                       WHERE PLAYERID2 = ?'''
+        con.execute(sql_update_game_detail2, (player_name, player_id))
+        con.commit()
+    finally:
+        con.close()
+        
+def update_admin(administrator_id, administrator_name, administrator_pw):
+    """更新一条记录到Admin表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''UPDATE Admin
+                    SET ADMINNAME = ?
+                        ,PASSWORD = ?
+                    WHERE ADMINID = ?'''
+        con.execute(sql, (administrator_name, administrator_pw, administrator_id))
+        con.commit()
+    finally:
+        con.close()
+        
+def update_game(game_id, player_id1, player_name1, player_id2, player_name2, location, duration, winner, loser):
+    """更新一条记录到GameDetail表"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''UPDATE GameDetail
+                    SET PLAYERID1 = ?
+                        ,PLAYERNAME1 = ?                    
+                        ,PLAYERID2 = ?
+                        ,PLAYERNAME2 = ?
+                        ,DETAILTEXT = ?
+                        ,TIME = ?
+                        ,WINNER = ?
+                    WHERE GAMEID = ?'''
+        con.execute(sql, (player_id1, player_name1, player_id2, player_name2, location, duration, winner, loser, game_id))
+        con.commit()
+    finally:
+        con.close()
+
+def delete_player(player_id):
+    """从Player表中删除一条记录"""
+    con = get_or_create_db(DBFILE)
+    try:
+        con.execute('PRAGMA foreign_keys = OFF;')
+        sql_update_game_detail = '''UPDATE GameDetail
+                                       SET PLAYERID1 = null
+                                           ,PLAYERNAME1 = null
+                                       WHERE PLAYERID1 = ?'''
+        con.execute(sql_update_game_detail, (player_id,))
+        
+        sql_update_game_detail2 = '''UPDATE GameDetail
+                                        SET PLAYERID2 = null
+                                            ,PLAYERNAME2 = null
+                                        WHERE PLAYERID2 = ?'''
+        con.execute(sql_update_game_detail2, (player_id,))
+        sql = '''DELETE FROM Player
+                    WHERE PLAYERID = ?'''
+        con.execute(sql, (player_id,))
+        con.commit()
+    finally:
+        con.execute('PRAGMA foreign_keys = ON;')        
+        con.close()
+        
+def delete_admin(administrator_id):
+    """从Admin表中删除一条记录"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''DELETE FROM Admin
+                    WHERE ADMINID = ?'''
+        con.execute(sql, (administrator_id,))
+        con.commit()
+    finally:
+        con.close()
+        
+def delete_game(game_id):
+    """从GameDetail表中删除一条记录"""
+    con = get_or_create_db(DBFILE)
+    try:
+        sql = '''DELETE FROM GameDetail
+                    WHERE GAMEID = ?'''
+        con.execute(sql, (game_id,))
+        con.commit()
+    finally:
+        con.close()
