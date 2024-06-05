41a10fd5bc33adf66fa5ecdc21938afc77236777
BUJIQI <145289312+BUJIQI@users.noreply.github.com>
2024-06-05 19:59:36 +0800

    Update login.py

--- tic-tac-toe/frontends/login.py
+++ tic-tac-toe/frontends/login.py
@@ -6,27 +6,27 @@ import data as db
 
 class LoginWindow(wx.Frame):
     def __init__(self, parent, title):
-        style = wx.CLOSE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.TAB_TRAVERSAL  
+        style = wx.CLOSE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.TAB_TRAVERSAL
         super(LoginWindow, self).__init__(parent, title=title, size=(800, 600), style=style)
         panel = wx.Panel(self, wx.ID_ANY)
-        
+
         userSizer = wx.BoxSizer(wx.HORIZONTAL)
         passwordSizer = wx.BoxSizer(wx.HORIZONTAL)
         btnSizer = wx.BoxSizer(wx.HORIZONTAL)
-        
+
         labelUser = wx.StaticText(panel, label="用户名:")
         self.inputTextUserName = wx.TextCtrl(panel)
-        
+
         labelPassword = wx.StaticText(panel, label="密码:")
         self.inputTextPassword = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
-        
+
         lblList = ['管理员', '玩家']
         self.rboxUserType = wx.RadioBox(panel, label='角色', choices=lblList)
-        self.rboxUserType.SetSelection(1) #默认选择玩家 
-        
+        self.rboxUserType.SetSelection(1)  # 默认选择玩家
+
         okBtn = wx.Button(panel, label="登录")
         cancelBtn = wx.Button(panel, label="取消")
-        
+
         topSizer = wx.BoxSizer(wx.VERTICAL)
         userSizer = wx.BoxSizer(wx.HORIZONTAL)
         passwordSizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -52,12 +52,12 @@ class LoginWindow(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.onLogin, okBtn)
         self.Bind(wx.EVT_BUTTON, self.onCancel, cancelBtn)
         self.Center()
-        
+
     def onLogin(self, e):
         username = self.inputTextUserName.GetValue()
         password = self.inputTextPassword.GetValue()
         UserType = self.rboxUserType.GetStringSelection()
-        
+
         if len(username.strip()) == 0:
             wx.MessageBox('请输入用户名！')
             self.inputTextUserName.SetFocus()
@@ -67,12 +67,9 @@ class LoginWindow(wx.Frame):
             self.inputTextPassword.SetFocus()
             return None
 
-        # 这里暂时写为当用户名密码不为空
-        
-                
- 
-        if db.login(username, password):  
-            wx.MessageBox('登录成功！')  
+
+        if db.login(username, password):
+            wx.MessageBox('登录成功！')
             if self.rboxUserType.GetSelection() == 0:
                 if username and password:
                     admin_main_frame = admin_main.TicTacToeAdminFrame(None, title='井字棋', username=username)
@@ -83,19 +80,17 @@ class LoginWindow(wx.Frame):
                 if username and password:
                     player_main_frame = player_main.TicTacToePlayerFrame(None, title='井字棋', username=username)
                     player_main_frame.Show()
-                    self.Close()# 这里可以跳转到游戏主界面或其他逻辑  
-        else:  
-            wx.MessageBox('登录失败，请检查您的用户名和密码！')  
+                    self.Close()  # 这里可以跳转到游戏主界面或其他逻辑
+        else:
+            wx.MessageBox('登录失败，请检查您的用户名和密码！')
 
-
-        
     def onCancel(self, e):
         self.Close(True)
-        
+
 
 if __name__ == '__main__':
     app = wx.App(redirect=False)
     frame = LoginWindow(None, "登录")
-    frame.CentreOnScreen() 
+    frame.CentreOnScreen()
     frame.Show()
     app.MainLoop()
