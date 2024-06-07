4f03b7c95106ee6c7b4ca9a7dfee63d21763f773
BUJIQI <145289312+BUJIQI@users.noreply.github.com>
2024-06-07 14:21:27 +0800

    Update player_main.py

--- tic-tac-toe/frontends/player_main.py
+++ tic-tac-toe/frontends/player_main.py
@@ -2,126 +2,71 @@ import wx
 import ui_play
 import game_main
 import player_check
-  
-class TicTacToePlayerFrame(wx.Frame):  
+
+
+class TicTacToePlayerFrame(wx.Frame):
     def __init__(self, parent, title, username):
-        style = wx.CLOSE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.TAB_TRAVERSAL  
-        super(TicTacToePlayerFrame, self).__init__(parent, title=title, size=(800, 600), style=style)  
-        self.CreateStatusBar()  
-        self.username = username  
-        self.SetStatusText('已登录用户: {}'.format(self.username))
+        style = wx.CLOSE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.TAB_TRAVERSAL
+        super(TicTacToePlayerFrame, self).__init__(parent, title=title, size=(800, 600), style=style)
+        self.CreateStatusBar()
+        self.username = username
 
         # 创建菜单栏  
-        menubar = wx.MenuBar()  
-          
+        menubar = wx.MenuBar()
+
         # 系统菜单  
-        sysmenu = wx.Menu()  
-        menuExit = sysmenu.Append(wx.ID_EXIT, "退出", "退出游戏")  
-        menubar.Append(sysmenu, "&系统")  
+        sysmenu = wx.Menu()
+        menuExit = sysmenu.Append(wx.ID_EXIT, "退出", "退出游戏")
+        menubar.Append(sysmenu, "&系统")
         menuReturnLogin = sysmenu.Append(wx.ID_ANY, "返回登录", "返回登录界面")
         self.Bind(wx.EVT_MENU, self.OnReturnLogin, menuReturnLogin)
 
         # 玩家菜单  
-        playermenu = wx.Menu()  
+        playermenu = wx.Menu()
         menuGame = playermenu.Append(wx.ID_ANY, "开始新游戏", "开始一局新的井字棋游戏")
-        menuView = playermenu.Append(wx.ID_ANY, "查看历史", "查看过去的游戏记录")  
-        menubar.Append(playermenu, "&玩家")  
-          
+        menuView = playermenu.Append(wx.ID_ANY, "查看历史", "查看过去的游戏记录")
+        menubar.Append(playermenu, "&玩家")
+
         # 关于菜单  
-        helpmenu = wx.Menu()  
-        menuAbout = helpmenu.Append(wx.ID_ABOUT, "关于", "关于井字棋游戏")  
-        menubar.Append(helpmenu, "&帮助")  
-          
+        helpmenu = wx.Menu()
+        menuAbout = helpmenu.Append(wx.ID_ABOUT, "关于", "关于井字棋游戏")
+        menubar.Append(helpmenu, "&帮助")
+
         # 绑定事件  
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Bind(wx.EVT_MENU, self.OnReturnLogin, menuReturnLogin)
-        self.Bind(wx.EVT_MENU, self.OnNewGame, menuGame)  
-        self.Bind(wx.EVT_MENU, self.OnViewHistory, menuView)  
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)  
-          
+        self.Bind(wx.EVT_MENU, self.OnNewGame, menuGame)
+        self.Bind(wx.EVT_MENU, self.OnViewHistory, menuView)
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+
         # 设置菜单栏  
-        self.SetMenuBar(menubar)  
-        self.Show()  
-        self.Center()  
-  
-    def OnExit(self, e):  
-        self.Close()  
-    
-    def OnReturnLogin(self, e):  
-    # 这里应该添加返回登录界面的逻辑  
+        self.SetMenuBar(menubar)
+        self.Show()
+        self.Center()
+
+    def OnExit(self, e):
+        self.Close()
+
+    def OnReturnLogin(self, e):
+        # 这里应该添加返回登录界面的逻辑  
         register_or_login_frame = register_or_login.MyFrame(None, title='井字棋')
         register_or_login_frame.Show()
         self.Close()
 
-    def OnNewGame(self, e):  
+    def OnNewGame(self, e):
         # 这里可以添加开始新游戏的逻辑  
         ui_play_frame = ui_play.MyFrame(None, title='井字棋', username=self.username)
         ui_play_frame.Show()
 
-  
-    def OnViewHistory(self, e):  
+    def OnViewHistory(self, e):
         # 这里可以添加查看历史游戏的逻辑  
-        player_check_frame = player_check.PlayerFrame(self, self.username, title='玩家查看')
+        player_check_frame = player_check.PlayerFrame(None)
         player_check_frame.Show()
-  
-    import wx
-import game_main
-import administrator_check
-  
-class TicTacToeAdminFrame(wx.Frame):  
-    def __init__(self, parent, title, username):  
-        style = wx.CLOSE_BOX | wx.CAPTION | wx.SYSTEM_MENU | wx.TAB_TRAVERSAL  
-        super(TicTacToeAdminFrame, self).__init__(parent, title=title, size=(800, 600), style=style)   
-        self.CreateStatusBar()  
-        self.username = username  
-          
-        # 创建菜单栏  
-        menubar = wx.MenuBar()  
-          
-        # 系统菜单  
-        sysmenu = wx.Menu()  
-        menuExit = sysmenu.Append(wx.ID_EXIT, "退出", "退出系统")  
-        menubar.Append(sysmenu, "&系统")  
-        menuReturnLogin = sysmenu.Append(wx.ID_ANY, "返回登录", "返回登录界面")  
-        # 管理者菜单（只有查看）  
-        adminmenu = wx.Menu()  
-        menuView = adminmenu.Append(wx.ID_ANY, "查看游戏", "查看所有游戏记录")  
-        menubar.Append(adminmenu, "&管理")  
-          
-        # 关于菜单  
-        helpmenu = wx.Menu()  
-        menuAbout = helpmenu.Append(wx.ID_ABOUT, "关于", "关于井字棋游戏")  
-        menubar.Append(helpmenu, "&帮助")  
-          
-        # 绑定事件  
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)  
-        self.Bind(wx.EVT_MENU, self.OnViewGames, menuView)  
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-        self.Bind(wx.EVT_MENU, self.OnReturnLogin, menuReturnLogin)
+        self.Close()
 
-        # 设置菜单栏  
-        self.SetMenuBar(menubar)  
-        self.Show()  
-        self.Center()  
-  
-    def OnExit(self, e):  
-        self.Close()  
-    
-    def OnReturnLogin(self, e):  
-    # 这里应该添加返回登录界面的逻辑  
-        register_or_login_frame = register_or_login.MyFrame(None, title='井字棋')
-        register_or_login_frame.Show()
-        self.Close()
-  
-    def OnViewGames(self, e):  
-        # 这里可以添加查看所有游戏记录的逻辑  
-        administrator_check_frame = administrator_check.UserWindow(None, title='登录')
-        administrator_check_frame.Show()
-        self.Close()
-  
     def OnAbout(self, e):
         description = '''
-           本游戏基于传统井字棋玩法，同时拓展了玩家查询历史对局及积分功能。
+          本游戏基于传统井字棋玩法，同时拓展了玩家查询历史对局及积分功能。
 
            玩家部分：
            开始新游戏：玩家-开始新游戏。
@@ -143,8 +88,5 @@ class TicTacToeAdminFrame(wx.Frame):
         dlg = wx.MessageDialog(self, description, "关于井字棋游戏", wx.OK | wx.ICON_INFORMATION)
         dlg.ShowModal()
         dlg.Destroy()
-  
-
-
 
 
